bcde2d6f49c65b80525c98e1d439b118c6c907b0
anastasiya.susloparova <anastasiya.susloparova@simbirsoft.com>
2024-08-07 22:19:08 +0300

    Add JsonPath test

--- src/test/java/TestPlaygroundLearnQA.java
+++ src/test/java/TestPlaygroundLearnQA.java
@@ -1,12 +1,42 @@
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestPlaygroundLearnQA {
-    @Test
-    public void testGetMethod() {
+   /* @Test
+    public void testRestAssured() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "Liz");
+
         Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
                 .andReturn();
         response.prettyPrint();
     }
+
+    */
+   @Test
+   public void testRestAssured() {
+       Map<String, String> params = new HashMap<>();
+       params.put("name", "Liz");
+
+       JsonPath response = RestAssured
+               .given()
+               .queryParams(params)
+               .get("https://playground.learnqa.ru/api/hello")
+               .jsonPath();
+
+       String name = response.get("answer2");
+       if (name == null) {
+           System.out.println("Key \"answer2\" is absent");
+       } else {
+           System.out.println(name);
+       }
+          }
 }
