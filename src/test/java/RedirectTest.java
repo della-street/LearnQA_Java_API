8f96b0cc71c508955964b3407d2f2b26c31ddbbb
anastasiya.susloparova <anastasiya.susloparova@simbirsoft.com>
2024-08-13 14:33:49 +0300

    add Get First Redirect Test

---
+++ src/test/java/RedirectTest.java
@@ -0,0 +1,23 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RedirectTest {
+    @Test
+    @DisplayName("Получение адреса первого редиректа")
+    public void getRedirect() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String redirect = response.getHeader("Location");
+        System.out.println(redirect);
+
+
+    }
+}
